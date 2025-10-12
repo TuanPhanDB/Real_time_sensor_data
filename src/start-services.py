4f0f9c690069808b8f4da6e991b30be101a4c7e7
TuanPhanDB <baotuanwork@gmail.com>
2025-10-12 15:08:15 +0300

    Add auto script

---
+++ src/start-services.py
@@ -0,0 +1,32 @@
+import subprocess
+import os
+
+def run_terminal(title, command):
+    subprocess.Popen([
+        "powershell",
+        "-NoExit",
+        f"-Command",
+        f"$Host.UI.RawUI.WindowTitle = '{title}'; {command}"
+    ],
+    creationflags=subprocess.CREATE_NEW_CONSOLE)
+
+if __name__ == "__main__":
+
+    base_dir = os.path.join(os.getcwd())
+    activate = ".\\env\\Scripts\\activate"
+
+    #Terminal 1: data_generator.py
+    cmd1 = f'Set-Location "{base_dir}"; {activate}; uvicorn data_generator:app --reload'
+    
+    #Terminal 2: producer.py
+    cmd2 = f'Set-Location "{base_dir}"; {activate}; py producer.py'
+
+    #Terminal 3: consumer.py
+    cmd3 = f'Set-Location "{base_dir}"; {activate}; py consumer.py'
+
+    print("Starting services....")
+    run_terminal("Data generator", cmd1)
+    run_terminal("Producer", cmd2)
+    run_terminal("Consumer", cmd3)
+
+    print("Services started")
